5de042d62b6ebde30118994a18edfc720940c149
maximombro <maximombro@gmail.com>
2022-01-29 17:45:01 -0500

    Added a go button to the fileSelect

--- imguiRenderer/components/fileSelect.py
+++ imguiRenderer/components/fileSelect.py
@@ -20,15 +20,31 @@ class FileSelectorComponent():
     def uiFileSelect(self):
         """
         Renders a File Select Ui.
+        The currently selected path is accessible at `fsFilepath`.
         """
         # Display the text input window
         imgui.begin(label="File Select", closable=False, flags=0)
 
-        # Top input bar
-        imgui.push_item_width(-1.0)
+        # Get the available size for the window
+        winSizeAvail = imgui.get_content_region_max()
+
+        # Calculate top bar size
+        barAddrW = (winSizeAvail[0] * .85)
+
+        # Show top bar
+        imgui.push_item_width(barAddrW)
         clicked, self.fsFilepath = imgui.input_text(label="", value=self.fsFilepath, buffer_length=256)
+        imgui.pop_item_width()
+
+        print(imgui.get_content_region_max())
+
+        # Show top bar go button
+        imgui.same_line()
+        if imgui.button(label="Go", width=-1):
+            pass
 
         # Directory content
+        imgui.push_item_width(-1.0)
         for f in self._fsFilelist:
             _, _ = imgui.selectable(f, False)
 
@@ -37,7 +53,6 @@ class FileSelectorComponent():
                     print("Double")
                 elif imgui.is_mouse_clicked():
                     print("Single")
-
         imgui.pop_item_width()
 
         imgui.end()
